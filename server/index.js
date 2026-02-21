4c264e6e07f5023d768a78a09a5093f6b0aec025
Wahid <wahid@gmail.com>
2026-02-21 16:27:33 +0600

    fix socket server

--- server/index.js
+++ server/index.js
@@ -126,7 +126,7 @@ io.on("connection", (socket) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 
